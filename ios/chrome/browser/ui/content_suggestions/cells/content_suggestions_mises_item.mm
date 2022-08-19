650afb84f27544339a35c93917d5d398e4cf96b1
freshlife001 <>
2022-08-19 18:09:49 +0800

    fix: ui minor fix

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_mises_item.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_mises_item.mm
@@ -105,6 +105,7 @@ NSURL * const kLink = [NSURL URLWithString:@"https://home.mises.site/"];
       _iconView.contentMode = UIViewContentModeScaleAspectFit;
     [_enterButton setTitle:kButtonTitle forState:UIControlStateNormal];
     [_enterButton setBackgroundColor:UIColorFromRGB(0x5D61FF)];
+    _enterButton.layer.cornerRadius = kButtonHeight / 2;
 
     [self.contentView addSubview:_containerView];
     [_containerView addSubview:_iconView];
